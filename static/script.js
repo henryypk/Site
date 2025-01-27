f230c610832c21858950e04b648141d7e5103f6c
IFMA | Campus Rosário <aluno@27IFMA0215.ifma.edu>
2025-01-27 12:11:59 -0300

    estive aqui

--- static/script.js
+++ static/script.js
@@ -3,5 +3,33 @@ const carousel = new bootstrap.Carousel('#myCarousel')
 function butao(){
     alert('Compra realizada com sucesso!');
   }
+  document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".itens");
+    const items = document.querySelectorAll(".produtos");
+    let activeFilter = null; // Para rastrear o filtro ativo
 
-  
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const filter = button.getAttribute("data-filter");
+
+            // Alternar o filtro ativo
+            if (activeFilter === filter) {
+                activeFilter = null; // Remover o filtro
+                showAllItems(); // Mostrar todos os itens
+            } else {
+                activeFilter = filter; // Atualizar o filtro ativo
+                filterItems(filter); // Filtrar itens
+            }
+        });
+    });
+
+    function filterItems(filter) {
+        items.forEach(item => {
+            if (item.classList.contains(filter)) {
+                fadeIn(item);
+            } else {
+                fadeOut(item);
+            }
+        });
+    }
+  })
